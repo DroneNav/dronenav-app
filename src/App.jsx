d224f799a81a57c793c9727603556f7313081bf9
Tony Dunbar <dunbar.tony@gmail.com>
2026-06-13 22:34:22 -0400

    Removed read-only mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,7 @@ import MapView from './components/MapView';
 
 function App() {
   return (
-    <MapView
-      readOnly={true}
-      siteId="019ea939-91e5-7617-9d17-4c7042a00674"
-    />
+    <MapView />
   );
 }
 
